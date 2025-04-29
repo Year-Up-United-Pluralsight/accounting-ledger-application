e440c5c7cc6660670b7a9ddac931b8fc3e403f35
dinganic03 <dinganic03@gmail.com>
2025-04-29 16:09:52 -0400

    Completed displayHomeScreen(); refined console output messages

--- src/main/java/com/pluralsight/Console.java
+++ src/main/java/com/pluralsight/Console.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Console {
@@ -7,10 +8,10 @@ public class Console {
     static Scanner scanner = new Scanner(System.in);
 
     // Displays home screen options; then displays the selected option
-    public static void displayHomeScreen(){
+    public static void displayHomeScreen() {
 
         // Displays home screen options; prompts user for choice
-        System.out.print("~ Welcome to the Accounting Ledger! How can we help you...\n" +
+        System.out.print("~ Welcome to the Accounting Ledger! How can I help you...\n" +
                 "   (D) Add a Deposit\n" +
                 "   (P) Make a Payment (Debit)\n" +
                 "   (L) View Ledger\n" +
@@ -20,7 +21,7 @@ public class Console {
         // Stores user's choice
         String input = scanner.nextLine();
 
-        // Executes the task based on user's choice
+        // Upon 'D' input, performs a deposit
         if (input.equals("D")) {
 
             // Echos user's selection
@@ -28,12 +29,13 @@ public class Console {
             printDelayedEllipsis();
 
             // Prompts for and stores info from user for a deposit
-            System.out.print("\nPlease enter the deposit information below: \n");
+            System.out.print("Please enter the deposit information below: \n");
 
             // Adds transaction (a deposit) to ledger
             Transaction.addTransaction(getUserTransaction());
         }
 
+        // Upon 'P' input, performs a payment
         else if (input.equals("P")) {
 
             // Echos user's selection
@@ -41,7 +43,7 @@ public class Console {
             printDelayedEllipsis();
 
             // Prompts for and stores info from user for a deposit
-            System.out.print("\nPlease enter the debit information below: \n");
+            System.out.print("Please enter the debit information below: \n");
 
             // Converts the transaction's amount to negative
             Transaction transaction = getUserTransaction();     // Prompts user for a transaction
@@ -52,26 +54,53 @@ public class Console {
             Transaction.addTransaction(transaction);
         }
 
+        // Upon 'L' input, displays ledger screen
         else if (input.equals("L")) {
 
+            // Echos user's selection
+            System.out.print("\nYou've selected \"View Ledger\"");
+            printDelayedEllipsis();
+
+            displayLedger();
         }
 
+        // Upon 'X' input, exits application
         else if (input.equals("X")) {
-
+            // Echos user choice
+            System.out.println("\n~ Thank you for stopping by, and have a wonderful rest of your day!");
         }
 
+        // Upon incorrect inputs, error message and re-displays options
         else {
 
+            // Checks if user wants to either 1) see options again or 2) exit
+            System.out.print("\nSorry! I'm not sure I understand what you mean by that. Would you like to try again? (Y/N): ");
+            String response = scanner.nextLine();
+
+            if (response.equals("Y")){
+
+                // Echos user choice
+                System.out.print("\nGreat! Let’s give it another go");
+                printDelayedEllipsis();
+
+                // Goes back to home screen
+                displayHomeScreen();
+            }
+
+            else {
+                // Echos user choice
+                System.out.println("\n~ Thank you for stopping by, and have a wonderful rest of your day!");
+            }
         }
 
-        }
+    }
 
     // Displays ledger options; then displays the selected option (all entries show the newest entry first)
     public static void displayLedger(){
 
         // Displays ledger options; prompts user for choice
-        System.out.println("Please select what you'd like to do next:" +
-                "(A) All" +
+        System.out.println("Please select what you'd like to do next:\n" +
+                "(A) All\n" +
                 "(D) Deposits\n" +
                 "(P) Payments\n" +
                 "(R) Reports\n" +
@@ -129,14 +158,17 @@ public class Console {
     // Displays entries that are payments
     private static void displayOnlyPayments(){}
 
-    // Helper func for introducing next screen
+// HELPER METHODS
+    // Adds delayed ellipsis for screen transitions
     private static void printDelayedEllipsis(){
         // Prints delayed ellipsis...
         try {
             for (int i = 0; i < 3; i++) {
-                System.out.print(".");
-                Thread.sleep(400);
+                System.out.print(".");     // Prints three periods (an ellipsis)
+                Thread.sleep(400);   // Delays printing of each period
             }
+
+            System.out.println("\n");          // Skips two lines
         }
         catch(InterruptedException e){
             System.out.println("Error");
